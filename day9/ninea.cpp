cfca8efa8ec3403d5298b6ffeaad5fc9e0e646f3
Viha123 <sviha195@gmail.com>
2023-06-20 19:29:18 -0700

    day 9 part a

--- day9/ninea.cpp
+++ day9/ninea.cpp
@@ -11,15 +11,17 @@
 #define N 3
 class IntcodeComputer{
     public:
-        IntcodeComputer(const std::vector<int>& program){ //takes in the "program/code" can continue to be vector
+        IntcodeComputer(const std::vector<long long>& program){ //takes in the "program/code" can continue to be vector
             this->initial_program = program;
             this->program = program;
+            relative_base = 0; //starts at 0
+
         }
         void reset(){
             program = initial_program; 
             complete = true;
             ip = 0;
-            relative_base = 0; //starts at 0
+            relative_base = 0;
 
         }
         int interpret(int ip, int param, int offset)
@@ -33,52 +35,47 @@ class IntcodeComputer{
                 return ip;
             }
             else{
-                return program[relative_base+offset];
+                return relative_base+offset;
             }
             
         }
-        int compute(const std::vector<int> &input)
+        long long compute()
         {    
             int mode = 0; //if mode = 0, then position mode, if mode = 1 then immediate mode
-            int output = -1;
+            long long output = -1;
             int input_ptr = 0;
             while(program[ip]!=99)
             {
                 //program[ip] is a max 5 digit number which needs to be parsed to be 3 digit position par and 2 digit ip
                 int op = program[ip]%100;
-                int x = program[ip];
-                int p1 = interpret(ip+1, (x/100)%10, program[ip+1]);
-                int p2 = interpret(ip+2, (x/1000)%10, program[ip+2]);
-                int p3 = interpret(ip+3, (x/10000)%10, program[ip+3]);
+                long long x = program[ip];
+                long long p1 = interpret(ip+1, (x/100)%10, program[ip+1]);
+                long long p2 = interpret(ip+2, (x/1000)%10, program[ip+2]);
+                long long p3 = interpret(ip+3, (x/10000)%10, program[ip+3]);
             
                 if(op== 1){
                     program[p3] = program[p1] + program[p2];
                     ip+=4;
                 }
-                else if(op == 2)
-                {
-                
+                else if(op == 2){
                     program[p3] = program[p1] * program[p2];
                     ip+=4;
-
                 }
                 
                 else if(op == 3)
                 {
-
-                    program[p1] = input[input_ptr];
-                    input_ptr+=1;
-                    ip+=2;
+                    int inputPosition;
+                    std::cin >> inputPosition;
+                    program[p1] = inputPosition;
+                    ip += 2;
                 }
-                else if(op==4)
-                {
+                else if(op==4){
                     output = program[p1]; //parameter
                     ip+=2;
-                    return output;
+                    std::cout << output << std::endl;
                 }
 
-                else if(op == 5)
-                {
+                else if(op == 5){
                     if(program[p1]!= 0)
                     {
                         ip = program[p2];
@@ -109,7 +106,7 @@ class IntcodeComputer{
                     ip+=4;
                 }
                 else if(op == 9){
-                    relative_base += p1;
+                    relative_base += program[p1];
                     ip+=2;
                 }
             
@@ -119,79 +116,36 @@ class IntcodeComputer{
         }
         bool isComplete() {return complete;}
     private:
-        std::vector<int> program;
-        std::vector<int> initial_program;
+        std::vector<long long> program;
+        std::vector<long long> initial_program;
         bool complete = false;
         int ip = 0;
         int relative_base;
 };
-//gonna have 5! different non repeating  combinations. need to use dfs/backtracking to sovle problem
-void dfs(std::unordered_set<int> hset, std::vector<std::vector<int>>& combinations,std::vector<int> vec){
-    //base case
-    if(hset.size() == 0){
-        //append vector to combinations
-        combinations.push_back(vec);
-        return;
-    }
-    for(auto element: hset){
-        std::vector<int> v2 = vec;
-        std::unordered_set<int> set2 = hset;
-        v2.push_back(element);
-        set2.erase(element);
-        dfs(set2,combinations,v2);
-    }
-}
 
-void convert_to_vector(std::vector<int> &outputs, std::string content)
+void convert_to_vector(std::vector<long long> &outputs, std::string content)
 {
     std::stringstream ss1(content);
     while (ss1.good())
     {
         std::string substr;
         std::getline(ss1, substr, ',');
-        int num = stoi(substr);
+        long long num = stoll(substr);
         outputs.push_back(num);
     }
 }
-int test_amplifier_configuration(IntcodeComputer& computer, std::vector<int>& phase_setting){
-    int initial = 0;
-    for(int i = 0; i < phase_setting.size(); i ++){
-        int phase = phase_setting[i];
-        initial = computer.compute(std::vector<int>{phase_setting[i], initial});
-        computer.reset();
-    }
-    return initial;
-}
+
 int main()
 {
-    std::fstream data("seven.txt");
+    std::fstream data("nine.txt");
     std::string str;
-    std::vector<int> input; 
+    std::vector<long long> input; 
     std::getline(data, str);
     convert_to_vector(input, str);
 
-    std::unordered_set<int> hset = {0,1,2,3,4}; //this will be passed into every dfs func
-    std::vector<std::vector<int>> combinations;
-    std::vector<int> vec; //this will be appended into combinations 
-    dfs(hset, combinations,vec); 
-    int max = INT_MIN;
     IntcodeComputer computer(input); 
 
+    computer.compute();
 
-    for(auto outer: combinations){
-    
-        int initial = test_amplifier_configuration(computer, outer);
-        if(initial > max){
-            max = initial;
-        }
-    }
-    std::cout<< max << std::endl;
-
-
-    //PART B
-        
-    //
-
-    
 }
 
