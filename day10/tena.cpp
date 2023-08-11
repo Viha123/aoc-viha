ee951e1657e53c42ee30319882d3cab2a2e86f83
Viha123 <sviha195@gmail.com>
2023-08-10 17:24:22 -0700

    day 10 a working

--- day10/tena.cpp
+++ day10/tena.cpp
@@ -62,7 +62,7 @@ bool calculate_dir(double myX, double myY, double thatX, double thatY){
     else if(thatX < myX){
         return false;
     }
-    else if(thatY > myY){
+    else if(thatY < myY){
         return true;
     }
     else{
@@ -110,6 +110,66 @@ struct compare
         }
     }
 };
+void get_laser_point(vector<point> &points, point &laserPoint){
+    int max = INT_MIN;
+
+    for(point ele: points){
+        // cout << ele.x << ele.y << endl;
+        //make a hashset of vec class will need to see how to make vec equal
+        vector<vec> vecs;
+        int count = 0;
+        for(point ele2: points){
+            if(ele2 == ele){
+                //do nothing
+            }
+            else{
+                vec key = {ele, ele2};
+                //check if key exists in the vector already
+                if (std::any_of(vecs.begin(), vecs.end(), compare(key))) {
+                    //do nothing
+                    //if key exists with the vector then find which key it collides with and chose which one will 
+                    //be in the thing
+                    
+                }
+                else {
+                    vecs.push_back(key);
+                    count++;
+                }
+            }
+            
+        }
+        if(count > max){
+            max = count;
+            laserPoint = ele;
+        }
+    }
+}
+vector<vec> hit_points(point p, vector<point> points){ //eventually turn hitpoint to .
+    vector<vec> hitpoints;
+    //start from top
+    for(point ele2: points){
+        if(ele2 == p){
+            //do nothing
+        }
+        else{
+            vec key = {p, ele2};
+            //check if key exists in the vector already
+            if (std::any_of(hitpoints.begin(), hitpoints.end(), compare(key))) {
+                //do nothing
+                //if equal you chose the one with the distance that is closer
+                // cout << ele2.x << " " << ele2.y << endl;
+                
+            }
+            else {
+                hitpoints.push_back(key);
+            }
+        }
+        
+    }
+    
+    return hitpoints;
+
+}
 int main(){
     vector<vector<char>> data;
     get_data(data);
@@ -128,36 +188,25 @@ int main(){
         // cout << endl;
     }
     //
-    int max = INT_MIN;
-    for(point ele: points){
-        // cout << ele.x << ele.y << endl;
-        //make a hashset of vec class will need to see how to make vec equal
-        vector<vec> vecs;
-        int count = 0;
-        for(point ele2: points){
-            if(ele2 == ele){
-                //do nothing
-            }
-            else{
-                vec key = {ele, ele2};
-                //check if key exists in the vector already
-                if (std::any_of(vecs.begin(), vecs.end(), compare(key))) {
-                    //do nothing
-                }
-                else {
-                    vecs.push_back(key);
-                    count++;
-                }
-            }
-            
-        }
-        if(count > max){
-            max = count;
-        }
+    // point laserPoint; 
+    // get_laser_point(points, laserPoint);
+    // cout << laserPoint.x << " " << laserPoint.y << endl;
+    //get vector that establishes all first round hit points and changes them from # to . 
+    //make vector into heap, if hit count less than 200 repeat step 1 and 2 and when count is greater than 200 pop until 200th element. 
+    
 
+    //TEST EXAMPLE
+    point test_point = {8,4};
+    vector<vec> ps = hit_points(test_point, points);
 
+    for(auto p : ps){
+        cout << p.thatP.x << " " << p.thatP.y << endl;
     }
-    cout << max << endl;
+
+    
+    //we have the laserpoint from which we want to start eliminating asteroids
+    //take all the points that laserpoint will hit (vectors) and then organize them by heaps,keep popping
+    //the heap 
     //TESTING == statement for vectors
     // point exp0 = {0,0};
     // point exp1 = {3,1};
@@ -168,8 +217,4 @@ int main(){
     // vector<vec> tests; 
     // tests.push_back(vec1);
     // cout << tests.find(vec2) << endl;
-
-
-    
-
 }
