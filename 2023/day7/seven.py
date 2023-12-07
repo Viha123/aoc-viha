c6e67e246c68ca4f576c22d9ec07aa34193a3b35
vihashah <viha.shah@sjsu.edu>
2023-12-06 23:34:51 -0800

    part b!

--- 2023/day7/seven.py
+++ 2023/day7/seven.py
@@ -25,15 +25,31 @@ high_card = 1 # 1 + 1 + 1 + 1 + 1
 
 def find_card_kind(hand):
     counts = dict()
+    num_j = hand.count("J")
     for letter in hand:
         c = hand.count(letter)
         counts[letter] = c
         if counts[letter] == 5:
             return five_kind
-        if counts[letter] == 4:
+        if counts[letter] == 4 and num_j == 1: #check if 4 can be upgraded
+            return five_kind
+        if counts[letter] == 4 and num_j == 0:
             return four_kind
+
+    #find max number of counts that are not J
+    max_counts = -1
+    max_element = 0
+    for e in counts:
+        if counts[e] > max_counts and e != "J":
+            max_element = e
+            max_counts = max(counts[e], max_counts)
+    #whatever max_counts is you wanna add j_num to that and make j num = 0
+    # print(max_element, max_counts,  num_j)
+    if num_j > 0:
+        counts["J"] = 0
+        counts[max_element] += num_j
+
     # for letter in hand: 
-    
     num_ones = 0
     for element in counts:
         if counts[element] == 1:
@@ -46,8 +62,15 @@ def find_card_kind(hand):
         case 2:
             return three_kind
         case 1:
+            #there can be 2 possibilities, it can have a 4 + 1 or a 2 + 2 + 1
+            for element in counts: #checks if any of them have a 4
+                if counts[element] == 4:
+                    return four_kind            
             return two_pair
         case 0:
+            for element in counts:
+                if counts[element] == 5:
+                    return five_kind
             return full_house
     
 def readInput():
@@ -72,11 +95,13 @@ def comparer(item1, item2): #does not change for part 2 J is now worth 1
 
 if __name__ == "__main__":
     data = readInput()
+    # print(find_card_kind("JJJJK"))
     all_data = []
     for item in data:
         all_data.append([find_card_kind(item[0]), item])
-    # print(all_data)    
+    # print(f"before sort: {all_data}")    
     all_data.sort(key=cmp_to_key(comparer))
+    # print(all_data)
     sum = 0
     for i, data in enumerate(all_data):
         sum += (i+1)*int(data[1][1])
