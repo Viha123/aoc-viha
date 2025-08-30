e9a27247b0b9279dfe1dfdeba35ebe638c7d78fc
vihashah <viha.shah@sjsu.edu>
2025-08-30 15:37:02 -0700

    slow way to do day 2

---
+++ 2024/day2/main.rs
@@ -0,0 +1,72 @@
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+fn read_file_as_lines(file_path: &str) -> io::Result<Vec<String>> {
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+
+    reader.lines().collect::<Result<Vec<String>, io::Error>>()
+}
+fn get_vec_from_line(line: &String) -> Vec<i32> {
+    let vec: Vec<i32> = line
+        .split_whitespace()
+        .filter_map(|x: &str| x.parse::<i32>().ok())
+        .collect();
+    return vec;
+}
+fn part1(lines: &Vec<String>) {
+    let mut n_safe = 0;
+    for line in lines {
+        let vec = get_vec_from_line(&line);
+        // println!("{}");
+        if check(&vec) {
+            n_safe += 1;
+        }
+    }
+    println!("{n_safe}");
+}
+fn check(vec: &[i32]) -> bool {
+    let increasing_check: Vec<&[i32]> = vec
+        .chunk_by(|x, y| (x <= y && i32::abs(x - y) >= 1 && i32::abs(x - y) <= 3))
+        .collect();
+    let decreasing_check: Vec<&[i32]> = vec
+        .chunk_by(|x, y| (x >= y && i32::abs(x - y) >= 1 && i32::abs(x - y) <= 3))
+        .collect();
+    if increasing_check.len() == 1 || decreasing_check.len() == 1 {
+        return true;
+    }
+    return false;
+}
+fn part2(lines: &Vec<String>) {
+    let mut n_safe = 0;
+    for line in lines {
+        let vec = get_vec_from_line(line);
+        let mut i = 0;
+        if check(&vec) {
+            n_safe += 1;
+        } else {
+            let mut found_safe = false;
+            while i < vec.len() && !found_safe {
+                // if i were to be removed
+                let new_vec: Vec<i32> = vec[0..i]
+                    .iter()
+                    .chain(vec[i + 1..].iter())
+                    .cloned()
+                    .collect();
+                if check(&new_vec) {
+                    n_safe += 1;
+                    found_safe = true;
+                } 
+                i += 1;
+            }
+        }
+    }
+    println!("{n_safe}");
+}
+fn main() -> std::io::Result<()> {
+    let file_path = "./input.txt";
+    let lines = read_file_as_lines(file_path)?;
+    part1(&lines);
+    part2(&lines);
+    Ok(())
+}
